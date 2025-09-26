e10783af8bacc74a13583bdec2a27157d83ab964
dbguswls030 <dbguswls030@gmail.com>
2025-09-27 03:39:09 +0900

    feat: 앨범 접근 구현

--- Projects/Presentation/Sources/Scene/MainView.swift
+++ Projects/Presentation/Sources/Scene/MainView.swift
@@ -1,9 +1,35 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var showPhotoPicker = false
+    @State private var selectedImage: UIImage?
+
     init() {}
 
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if let image = selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            } else {
+                Text("No Image Selected")
+                    .foregroundStyle(.gray)
+                    .frame(width: 300, height: 300)
+                    .border(.gray, width: 1)
+            }
+
+            Button("open PHPicker") {
+                showPhotoPicker = true
+            }
+            .padding()
+            .background(.purple)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+        }
+        .sheet(isPresented: $showPhotoPicker) {
+            PHPhotoPickerView(selectedImage: $selectedImage)
+        }
     }
 }

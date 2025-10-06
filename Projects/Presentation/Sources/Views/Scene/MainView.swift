1104e4709d98d054aecbec2f78c0c059853b5e59
dbguswls030 <dbguswls030@gmail.com>
2025-10-06 10:54:55 +0900

    refactor: NavigationStack 추가 및 커스텀 네비게이션바 삭제

--- Projects/Presentation/Sources/Views/Scene/MainView.swift
+++ Projects/Presentation/Sources/Views/Scene/MainView.swift
@@ -11,26 +11,50 @@ struct MainView: View {
     init() {}
 
     var body: some View {
-        VStack(spacing: 0) {
-            SettingView(
-                isEditing: isEditing,
-                next: $next
-            )
-            if let image = selectedImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .clipped()
-            } else {
-                Text("No Image Selected")
-                    .foregroundStyle(.gray)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationStack {
+            VStack(spacing: 0) {
+                if let image = selectedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .clipped()
+                } else {
+                    Text("No Image Selected")
+                        .foregroundStyle(.gray)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                PhotoPickerView(showPhotoPicker: $showPhotoPicker)
             }
-            PhotoPickerView(showPhotoPicker: $showPhotoPicker)
-        }
-        .sheet(isPresented: $showPhotoPicker) {
-            PHPhotoPickerView(selectedImage: $selectedImage)
+            .sheet(isPresented: $showPhotoPicker) {
+                PHPhotoPickerView(selectedImage: $selectedImage)
+            }
+            .background(.black)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("이미지 선택")
+                        .foregroundStyle(.white)
+                }
+                if isEditing {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            selectedImage = nil
+                        } label: {
+                            Image(systemName: "xmark")
+                        }
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            next = true
+                        } label: {
+                            Image(systemName: "arrow.right")
+                                .foregroundStyle(.white)
+                        }
+                        .disabled(!isEditing)
+                    }
+                }
+            }
         }
     }
 }

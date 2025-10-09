0273ec8d93b890cf6d5e0a9c59b4209978293b6e
dbguswls030 <dbguswls030@gmail.com>
2025-10-09 20:21:44 +0900

    feat: 편집 뷰 네비게이션 바 및 배경 설정

--- Projects/Presentation/Sources/Views/Scene/Edit/EditView/EditView.swift
+++ Projects/Presentation/Sources/Views/Scene/Edit/EditView/EditView.swift
@@ -1,11 +1,34 @@
 import SwiftUI
 
 struct EditView: View {
+    @State private var save = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.black)
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("편집")
+                    .foregroundStyle(.white)
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    save = true
+                } label: {
+                    Image(systemName: "tray.and.arrow.down")
+                        .foregroundStyle(.white)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    EditView()
+    NavigationStack {
+        EditView()
+    }
 }
